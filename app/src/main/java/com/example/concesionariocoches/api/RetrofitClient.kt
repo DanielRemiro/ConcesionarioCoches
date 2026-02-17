71b0640db9cf3eb6802fb934a717f837ca70b99d
Daniel Remiro <remirolaguna23@iespabloserrano.com>
2026-02-17 09:29:18 +0100

    upd

--- app/src/main/java/com/example/concesionariocoches/api/RetrofitClient.kt
+++ app/src/main/java/com/example/concesionariocoches/api/RetrofitClient.kt
@@ -4,7 +4,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
-    private const val BASE_URL = "http://10.0.2.2:3000/"
+    private const val BASE_URL = "https://my-json-server.typicode.com/DanielRemiro/vehiculos/"
 
     val instance: ConcesionarioApi by lazy {
         Retrofit.Builder()
